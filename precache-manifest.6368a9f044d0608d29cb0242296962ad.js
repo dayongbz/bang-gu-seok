676ec1f0741c2e430fffc9700c24c6577aa4fa0f
dayongbz <dayongbz@gmail.com>
2019-08-20 09:08:22 +0900

    Updates

---
+++ precache-manifest.6368a9f044d0608d29cb0242296962ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce5254a2bfe6d75e853e79c94f1c5cd",
+    "url": "/bang-gu-seok/index.html"
+  },
+  {
+    "revision": "5471ed0fbec8b24df66f",
+    "url": "/bang-gu-seok/static/css/main.a8b6c546.chunk.css"
+  },
+  {
+    "revision": "3ec832b5f7711473009d",
+    "url": "/bang-gu-seok/static/js/2.166bb6d2.chunk.js"
+  },
+  {
+    "revision": "5471ed0fbec8b24df66f",
+    "url": "/bang-gu-seok/static/js/main.51c861bd.chunk.js"
+  },
+  {
+    "revision": "80655267332843ccec92",
+    "url": "/bang-gu-seok/static/js/runtime~main.ddac98f9.js"
+  }
+]);
